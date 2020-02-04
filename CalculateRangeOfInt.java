92457a78ce4c81eaedf43a3abb124c1cfe6a0064
BINGNAN, CHEN <59923237+r1991108@users.noreply.github.com>
2020-02-05 00:48:29 +0900

    check the limited range of integer

---
+++ CalculateRangeOfInt.java
@@ -0,0 +1,18 @@
+
+public class CalculateRangeOfInt {
+
+	public static void main(String[] args) {
+
+		int x=0;
+
+		while(x>=0) {
+			x++;
+		}
+		System.out.println(x);
+		x--;
+		System.out.println(x);
+		x++;
+		System.out.println(x);
+	}
+
+}
