ea2774483f40c24c3897f965b47d58702d069718
BINGNAN, CHEN <59923237+r1991108@users.noreply.github.com>
2020-02-05 00:57:30 +0900

    check the limited range of integer

--- CalculateRangeOfInt.java
+++ CalculateRangeOfInt.java
@@ -3,14 +3,14 @@ public class CalculateRangeOfInt {
 
 	public static void main(String[] args) {
 
-		int x=0;
+		int x = 0;
 
-		while(x>=0) {
+		while (x >= 0) {
 			x++;
 		}
-		System.out.println("minimum = "+x);
+		System.out.println("minimum = " + x);
 		x--;
-		System.out.println("maximum = "+x);
+		System.out.println("maximum = " + x);
 
 	}
 
